72b7e44de556956878bebd483e4aa64cb2206821
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-07-13 18:09:26 +0200

    Create Product Model and added POST method for saving Product in MongoDB

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 
-mongoose.connect("mongodb+srv://kyrylobulyk:12345qwert@kyrylobulyk.m7jdr88.mongodb.net/Node-API?retryWrites=true&w=majority&appName=kyrylobulyk")
-.then(() => {
+app.use(express.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://kyrylobulyk:12345qwert@kyrylobulyk.m7jdr88.mongodb.net/Node-API?retryWrites=true&w=majority&appName=kyrylobulyk"
+  )
+  .then(() => {
     console.log("Connected to database!");
-})
-.catch(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
     console.log("Connection failed!");
-})
+  });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+app.get("/", (req, res) => {
+  res.send("Hello from NodeJs API Hello");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello from NodeJs API Hello');
-})
+app.post("/api/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});

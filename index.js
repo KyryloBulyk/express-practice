aa786a0c9c0384f247a5b671dde274c176549673
Kyrylo Bulyk <kyrylo.bulyk@gmail.com>
2024-07-13 13:01:15 +0200

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect("mongodb+srv://kyrylobulyk:12345qwert@kyrylobulyk.m7jdr88.mongodb.net/Node-API?retryWrites=true&w=majority&appName=kyrylobulyk")
+.then(() => {
+    console.log("Connected to database!");
+})
+.catch(() => {
+    console.log("Connection failed!");
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello from NodeJs API');
+    res.send('Hello from NodeJs API Hello');
 })
